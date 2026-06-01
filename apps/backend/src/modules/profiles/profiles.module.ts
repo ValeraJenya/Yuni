cad35e94fe66afbf1d9422efc919997592bf1e18
ValeraJenya <arklegit_13@mail.ru>
2026-06-01 20:51:26 +0400

    fix(profiles): исправить проверку MVP профиля

--- apps/backend/src/modules/profiles/profiles.module.ts
+++ apps/backend/src/modules/profiles/profiles.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { ProfilesController } from './profiles.controller';
 import { ProfilesService } from './profiles.service';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [ProfilesController],
   providers: [ProfilesService],
 })
